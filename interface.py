2f6888b14efb6063e413489c2dec66ce0ee04df6
etiennedub <etiennedubeau@hotmail.com>
2015-11-29 15:07:24 -0500

    Tous les coordonees sont en (i,j)

--- interface.py
+++ interface.py
@@ -13,15 +13,13 @@ class classInterface(reseau):
         turtle.setup (width=x, height=y, startx=0, starty=0)#sert a juster la grandeur de la fenetre mais marche pas pour moi
         turtle.Screen().onkey(self.help,'m')
 
-    def click(self,x,y):
-        self.drawCircle(x,y,'red')
 
     def help(self):
         print('-------Menu------')
         print('m pour afficher le menu')
         print('v pour....')
 
-    def drawLine(self,color,coord1,coord2): #coordX accepte un tuple (column,line,"player") et dessine une ligne horizontal ou vertical de coor1 a coord2
+    def drawLine(self,color,coord1,coord2): #coordX accepte un tuple (i,j,"player") et dessine une ligne horizontal ou vertical de coor1 a coord2
         if coord1[2] == coord2[2] and coord2[2] == "you":
             turtle.goto(38+coord1[0]*25,87-25*coord1[1])
         elif coord1[2] == coord2[2] and coord2[2] == "enemy":
@@ -31,25 +29,25 @@ class classInterface(reseau):
             return 0
         turtle.pensize(20)
         turtle.pencolor(color)
-        if coord1[0] == coord2[0]: #Vertical
+        if coord1[1] == coord2[1]: #Vertical
             turtle.pendown()
             turtle.setheading(270)
-            turtle.fd((coord2[1]-coord1[1])*25)
-        elif coord1[1] == coord2[1]: #horizontal
+            turtle.fd((coord2[0]-coord1[0])*25)
+        elif coord1[0] == coord2[0]: #horizontal
             turtle.pendown()
             turtle.setheading(0)
-            turtle.fd((coord2[0]-coord1[0])*25)
+            turtle.fd((coord2[1]-coord1[1])*25)
         else:
             print('Ligne non Hori ou Vert')
             return 0
         turtle.penup()
         return 1
 
-    def setCoord1(self,x,y): #set coord1 (colum,line,"player")
+    def setCoord1(self,x,y): #set coord1 (i,j,"player")
         turtle.Screen().onscreenclick(None)
-        column,line,player = self.position(x,y)
-        if column != player:
-            self.coord1 = (column,line,player)
+        i,j,player = self.position(x,y)
+        if j != player:
+            self.coord1 = (i,j,player)
             return self
         else:
             print("Mauvaise coordone")
@@ -57,11 +55,9 @@ class classInterface(reseau):
 
     def setCoord2(self,x,y):
         turtle.Screen().onscreenclick(None)
-        column,line,player = self.position(x,y)
-        if column != player:
-            self.coord2 = (column,line,player)
-            #self.setCoord = 1
-            #self.drawLine("gray",self.coord1,self.coord2)
+        i,j,player = self.position(x,y)
+        if j != player:
+            self.coord2 = (i,j,player)
         else:
             print("Mauvaise coordone")
             turtle.Screen().onscreenclick(self.setCoord2)
@@ -72,20 +68,20 @@ class classInterface(reseau):
             return(0,0,0)
         elif x < 0:
             player = "enemy"
-            column = 12 + int((int(x)-50)/25)
-            line = int((abs(int(y)-100))/25)
-            return(column,line,player)
+            j = 12 + int((int(x)-50)/25)
+            i = int((abs(int(y)-100))/25)
+            return(i,j,player)
         else:
             player = "you"
-            column = int((int(x)-25)/25)  #Position collone de 0 a 9
-            line = int((abs(int(y)-100))/25)
-            return(column,line,player)
+            j = int((int(x)-25)/25)  #Position collone de 0 a 9
+            i = int((abs(int(y)-100))/25)
+            return(i,j,player)
 
     def drawCircle(self,position,color): #entrer collonne et line pour dessiner un cercle au centre
-        column,line,player = position
+        i,j,player = position
         if player == "you":
-            turtle.goto(38+column*25,88-25*line)
+            turtle.goto(38+j*25,88-25*i)
             turtle.dot(20,color)
         if player == "enemy":
-            turtle.goto(-262+(25*column),88-25*line)
+            turtle.goto(-262+(25*j),88-25*i)
             turtle.dot(20,color)
